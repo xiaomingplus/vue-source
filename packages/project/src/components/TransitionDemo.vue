009e19a0ccafe8f986a5b88c24fb57d4a817c53c
guobaoyang <guobaoyang@tencent.com>
2018-08-22 16:53:03 +0800

    for add transition demo

---
+++ packages/project/src/components/TransitionDemo.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="demo">
+        <button v-on:click="show = !show">
+            Toggle
+        </button>
+        <transition name="fade">
+            <p v-if="show">hello</p>
+        </transition>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TransitionDemo',
+        props: {
+            msg: String
+        },
+        data: function () {
+            return {
+                show: true
+            }
+        }
+
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity .5s;
+    }
+
+    .fade-enter,
+    .fade-leave-to {
+        opacity: 0;
+    }
+</style>
---
+++ packages/project/src/views/Transition.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <TransitionDemo />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TransitionDemo from '@/components/TransitionDemo'
+
+export default {
+  name: 'transition',
+  components: {
+    TransitionDemo
+  }
+}
+</script>
